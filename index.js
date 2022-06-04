47eb0e599c2dc18a339e74b665fd20d5caccc97e
James Hassett <jhassett189@gmail.com>
2022-06-03 23:49:36 -0400

    commit tree 3b8d06aaff51b0a7da44d52473131b8e09f78471

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 31
+const num2 = 2
+let multiply = num1 * num2
 
-// Write your code here
+const num3 = 12
+const num4 = 3
+let mod = num3 / num4
+
+let max = 20
+Math.max(18, 19, 20)
+
+let random = 1
+Math.floor(Math.random() * 10) + 1
+
